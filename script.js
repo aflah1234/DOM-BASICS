ac8f291bd55d12e089142243a2318590a7f6f09b
unknown <aflahkondoth@gmail.com>
2024-08-20 20:37:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changelogo() {
+    const logo = document.getElementById('logo');
+    logo.src = './image/google png.png';
+}
+
+function reset() {
+    const logo = document.getElementById('logo');
+    logo.src = './image/entri-removebg-preview.png';
+}
